e41096023ee141421f6ba3a8e7bf42afcb5ad4ec
khalidtoak <32118762+Khalidtoak@users.noreply.github.com>
2018-07-27 18:39:43 +0100

    init project

---
+++ app/src/main/java/com/korahackathon/showmolu/www/shomolucom/Authentication/SignUp.java
@@ -0,0 +1,15 @@
+package com.korahackathon.showmolu.www.shomolucom.Authentication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.korahackathon.showmolu.www.shomolucom.R;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
---
+++ app/src/main/java/com/korahackathon/showmolu/www/shomolucom/DashBoard/CreateNewCardActivity.java
@@ -0,0 +1,15 @@
+package com.korahackathon.showmolu.www.shomolucom.DashBoard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.korahackathon.showmolu.www.shomolucom.R;
+
+public class CreateNewCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new_card);
+    }
+}
